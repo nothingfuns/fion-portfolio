9b16d29548531a9b92cd1efed24aecfd482f94d5
nothingfuns <goyfion@gmail.com>
2026-08-17 00:18:42 +0800

    Edit images, styling issues, 66 days content
    images were previously poorly cropped - replaced directly from figma
    
    styling issues that were not similar to figma

--- js/partials.js
+++ js/partials.js
@@ -23,7 +23,7 @@ function siteFooter() {
     <div class="container">
       <span class="footer-talk">Let's Talk!</span>
       <div class="footer-contact">
-        <a href="https://www.linkedin.com/" target="_blank" rel="noopener" aria-label="LinkedIn">
+        <a href="https://www.linkedin.com/in/goykaixuan/" target="_blank" rel="noopener" aria-label="LinkedIn">
           <svg viewBox="0 0 24 24"><path d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1s2.48 1.12 2.48 2.5zM.5 8h4V23h-4V8zm7.5 0h3.8v2.05h.05c.53-1 1.83-2.05 3.77-2.05 4.03 0 4.78 2.65 4.78 6.1V23h-4v-6.9c0-1.65-.03-3.77-2.3-3.77-2.3 0-2.65 1.8-2.65 3.65V23h-4V8z"/></svg>
         </a>
         <a href="tel:+6597275303">+65 9727 5303</a>
